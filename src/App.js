30139df9d3859b41a66568408cde3870235e5096
ManhBV <buivanmanh120799@gmail.com>
2024-02-19 17:58:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,30 @@
 import styled from 'styled-components';
+import logo from '../logo.svg';
+import { NavLink } from 'react-router-dom';
+import Nav from './Nav';
 
 const Header = () => {
-  return <ParentHeader>12313</ParentHeader>;
+  return (
+    <ParentHeader>
+      <NavLink to="/">
+        <img className="logo" src={logo} alt="LOGO" />
+      </NavLink>
+      <Nav/>
+    </ParentHeader>
+  );
 };
 
 const ParentHeader = styled.header`
   padding: 0 2rem;
   height: 5rem;
-  background-color: #000;
+  background-color: #f6f8fa;
   display: flex;
   justify-content: space-between;
   align-items: center;
   position: relative;
 
   .logo {
-    height: 8rem;
+    height: 5rem;
     /* width: 4rem; */
   }
 `;
